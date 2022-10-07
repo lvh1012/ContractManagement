cd9cc1c56412f0886a3fb1880a4632d0f1ad6b7f
hoanglv <lehoang99.18xt@gmail.com>
2022-10-07 23:59:56 +0700

    test

---
+++ Backend/API/API/Model/BaseResult.cs
@@ -0,0 +1,24 @@
+﻿namespace API.Model
+{
+    public class BaseResult
+    {
+        public bool Success { get; set; } = true;
+        public int Status { get; set; } = StatusCodes.Status200OK;
+        public object Data { get; set; }
+        public string? Message { get; set; }
+        public string? ErrorInFile { get; set; }
+        public string? ErrorMessage { get; set; }
+        public Exception? Exception { get; set; }
+
+        public BaseResult(bool success, int status, object data, string? message, string? errorInFile, string? errorMessage, Exception? exception)
+        {
+            Success = success;
+            Status = status;
+            Data = data;
+            Message = message;
+            ErrorInFile = errorInFile;
+            ErrorMessage = errorMessage;
+            Exception = exception;
+        }
+    }
+}

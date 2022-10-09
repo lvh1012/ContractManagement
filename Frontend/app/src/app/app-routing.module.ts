77d5a5bdb585bb9d6176f9235d2c6fa198b2f49d
hoanglv <lehoang99.18xt@gmail.com>
2022-10-09 23:27:00 +0700

    test FE

--- Frontend/app/src/app/app-routing.module.ts
+++ Frontend/app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { ContractComponent } from './contract/contract.component';
+import { CustomerComponent } from './customer/customer.component';
+import { ProductComponent } from './product/product.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'contract', pathMatch: 'full' },
+  { path: 'product', component: ProductComponent },
+  { path: 'customer', component: CustomerComponent },
+  { path: 'contract', component: ContractComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
